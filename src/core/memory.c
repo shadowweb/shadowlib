4aecec4dbc4696c92e9ecea74061cd4784b94cc5
Ella Baron <ellabaron@pobox.com>
2014-10-23 22:47:52 -0700

    Added cache line aligned allocation

--- src/core/memory.c
+++ src/core/memory.c
@@ -1,24 +1,28 @@
+#include "core/memory.h"
+
 #include <stdlib.h>
 #include <string.h>
-#include "memory.h"
+#include <unistd.h>
 
-static swMemoryManager defaultManager = {malloc, calloc, realloc, free};
+static swMemoryManager defaultManager = {malloc, calloc, realloc, free, posix_memalign};
 static swMemoryManager *currentManager = &defaultManager;
 
-swMemoryMallocFunction  *swMemoryMalloc   = malloc;
-swMemoryCallocFunction  *swMemoryCalloc   = calloc;
-swMemoryReallocFunction *swMemoryRealloc  = realloc;
-swMemoryFreeFunction    *swMemoryFree     = free;
+swMemoryMallocFunction       *swMemoryMalloc       = malloc;
+swMemoryCallocFunction       *swMemoryCalloc       = calloc;
+swMemoryReallocFunction      *swMemoryRealloc      = realloc;
+swMemoryFreeFunction         *swMemoryFree         = free;
+swMemoryAlignMallocFunction  *swMemoryAlignMalloc  = posix_memalign;
 
 void swMemoryManagerSet(swMemoryManager *manager)
 {
   if (manager)
   {
-    currentManager   = manager;
-    swMemoryMalloc   = currentManager->malloc;
-    swMemoryCalloc   = currentManager->calloc;
-    swMemoryRealloc  = currentManager->realloc;
-    swMemoryFree     = currentManager->free;
+    currentManager       = manager;
+    swMemoryMalloc       = currentManager->malloc;
+    swMemoryCalloc       = currentManager->calloc;
+    swMemoryRealloc      = currentManager->realloc;
+    swMemoryFree         = currentManager->free;
+    swMemoryAlignMalloc  = currentManager->alignMalloc;
   }
 }
 
@@ -40,6 +44,19 @@ void *swMemoryDuplicate(void *src, size_t size)
     rtn = swMemoryMalloc(size);
     if (rtn)
       memcpy(rtn, src, size);
+  }
+  return rtn;
+}
+
+static size_t cacheSize = 64;
+
+void *swMemoryCacheAlignMalloc(size_t size)
+{
+  void *rtn = NULL;
+  if (cacheSize || ((cacheSize = sysconf(_SC_LEVEL1_DCACHE_LINESIZE)) > 0))
+  {
+    if (swMemoryAlignMalloc(&rtn, cacheSize, size) != 0)
+      rtn = NULL;
   }
   return rtn;
 }
--- src/core/memory.h
+++ src/core/memory.h
@@ -7,14 +7,15 @@ typedef void *(swMemoryMallocFunction)(size_t size);
 typedef void *(swMemoryCallocFunction)(size_t nmemb, size_t size);
 typedef void *(swMemoryReallocFunction)(void *ptr, size_t size);
 typedef void (swMemoryFreeFunction)(void *ptr);
-
+typedef int (swMemoryAlignMallocFunction)(void **memptr, size_t alignment, size_t size);
 
 typedef struct swMemoryManager
 {
-  swMemoryMallocFunction   *malloc;
-  swMemoryCallocFunction   *calloc;
-  swMemoryReallocFunction  *realloc;
-  swMemoryFreeFunction     *free;
+  swMemoryMallocFunction      *malloc;
+  swMemoryCallocFunction      *calloc;
+  swMemoryReallocFunction     *realloc;
+  swMemoryFreeFunction        *free;
+  swMemoryAlignMallocFunction *alignMalloc;
 } swMemoryManager;
 
 void swMemoryManagerSet(swMemoryManager *manager);
@@ -27,5 +28,7 @@ extern swMemoryReallocFunction *swMemoryRealloc;
 extern swMemoryFreeFunction    *swMemoryFree;
 
 void *swMemoryDuplicate(void *src, size_t size);
+void *swMemoryCacheAlignMalloc(size_t size);
+// size_t swMemoryL1DSizeGet();
 
 #endif // SW_CORE_MEMORY_H
